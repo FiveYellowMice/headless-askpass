ff4078cb89d4266ef165038d894b60a8b17fa0da
FiveYellowMice <hkz85825915@gmail.com>
2026-05-18 04:56:11 +1000

    Prefix client error messages

--- client.go
+++ client.go
@@ -14,7 +14,7 @@ func RunClient(prompt string) {
 	// Connect to the server
 	conn, err := net.Dial("unix", path)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error connecting to server: %v\n", err)
+		fmt.Fprintf(os.Stderr, "headless-askpass: Error connecting to server: %v\n", err)
 		fmt.Fprintf(os.Stderr, "Make sure to start the server first.\n")
 		os.Exit(1)
 	}
@@ -23,7 +23,7 @@ func RunClient(prompt string) {
 	// Send the prompt to the server
 	_, err = fmt.Fprintf(conn, "%s\n", prompt)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error sending prompt to server: %v\n", err)
+		fmt.Fprintf(os.Stderr, "headless-askpass: Error sending prompt to server: %v\n", err)
 		os.Exit(1)
 	}
 
@@ -34,7 +34,7 @@ func RunClient(prompt string) {
 	reader := bufio.NewReader(conn)
 	response, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading response from server: %v\n", err)
+		fmt.Fprintf(os.Stderr, "headless-askpass: Error reading response from server: %v\n", err)
 		os.Exit(1)
 	}
 
